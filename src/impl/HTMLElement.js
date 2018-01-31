d3b0a2af4d586453ffe8f1cd5dd29e02ab56261c
Cristian Petruta <crispet@microsoft.com>
2018-01-30 23:39:27 -0800

    Hook-up HtmlVideoElement to native implementation

--- src/impl/HTMLElement.js
+++ src/impl/HTMLElement.js
@@ -1644,10 +1644,47 @@ defineLazyProperty(impl, "HTMLAudioElement", function() {
 defineLazyProperty(impl, "HTMLVideoElement", function() {
     function HTMLVideoElement(doc, localName, prefix) {
         impl.HTMLMediaElement.call(this, doc, localName, prefix);
+
+        this.native = new holographic.nativeInterface.video.createVideo();
+
+        this.nativeCallback = function(type) {
+            if (type === 'load' && arguments.length > 2) {
+                this.width = this.videoWidth = arguments[1];
+                this.height = this.videoHeight = arguments[2];
+
+                holographic.nativeInterface.eventing.removeCallback(this.native);
+
+                this._readyState = HAVE_ENOUGH_DATA;
+
+                var loadEvent = this.ownerDocument.createEvent("Event");
+                loadEvent.initEvent("canplaythrough", true, true);
+                this.dispatchEvent(loadEvent);
+            }
+        };
+
+        Object.defineProperty(this, "src", {
+            set: function setSrc(value) {
+                holographic.nativeInterface.eventing.setCallback(this.native, this.nativeCallback.bind(this));
+                holographic.nativeInterface.video.setVideoSource(this.native, value);
+            },
+            get: function getSrc() {
+                return this.source;
+            }
+        });
+
+        Object.defineProperty(this, "readyState", {
+            get: function getReadyState() {
+                return this._readyState;
+            }
+        });
     }
 
     HTMLVideoElement.prototype = O.create(impl.HTMLMediaElement.prototype, {
         _idlName: constant("HTMLVideoElement"),
+
+        getData: constant(function getData() {
+            return this.native;      
+        }),
     });
 
     // impl.Element.reflectURLAttribute(HTMLVideoElement,"poster");
